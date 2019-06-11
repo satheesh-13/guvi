0c3f2e319769a241ade33a09e600dce8044d8d4e
satheesh-13 <51689369+satheesh-13@users.noreply.github.com>
2019-06-11 11:39:35 +0530

    Create var1.c

---
+++ var1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,i,j,a[100],temp;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                printf("%d ",a[i]);
+                a[i]=0;
+            }
+        }
+    }
+}
