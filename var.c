bbd822d37ccc6f09078245ce50ed2be46e078e77
satheesh-13 <51689369+satheesh-13@users.noreply.github.com>
2019-06-11 11:33:22 +0530

    Create var.c

---
+++ var.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,i,j,a[100],temp;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]<a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("%d",a[k-1]);
+}
