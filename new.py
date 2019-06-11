6fe1473762a042b08527c9f7bea7d6885b23d606
satheesh-13 <51689369+satheesh-13@users.noreply.github.com>
2019-06-11 14:44:16 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,8 +1,8 @@
 n=int(input())
-l1=list()
+l2=list()
 for x in range(n):
-	l2=list(map(int,input().split()))
-	l1=l1+l2
-l1=sorted(l1)
-for x in l1:
+	l3=list(map(int,input().split()))
+	l2=l2+l3
+l2=sorted(l2)
+for x in l2:
 	print(x,end=" ")
